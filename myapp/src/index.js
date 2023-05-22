282a2605193d8ea1c45e4afa627f0aa383a11136
AlexDolz <aleksejdolznikov3@gmail.com>
2023-05-22 12:43:47 +0200

    hashrouter

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter hashType='hashbang'>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
