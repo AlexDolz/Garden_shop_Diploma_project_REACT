e97d3da652c1dde05df61f77458cb1f35cb56a33
AlexDolz <aleksejdolznikov3@gmail.com>
2023-06-08 15:25:07 +0200

    animation

--- myapp/src/components/Head/Head.jsx
+++ myapp/src/components/Head/Head.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import s from './Head.module.css';
 import Button from '../UI/Button/Button';
 import banner_img from './media/banner_img.png';
 import { Link } from 'react-scroll';
 
 const Head = () => {
+  const [background, setBackground] = useState('#a1e2eb');
+
+  useEffect(() => {
+    const interval = setInterval(changeBackground, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const changeBackground = () => {
+    const gradients = ['#a1e2eb', '#fbd3e9', '#f9e17d', '#b8f2a4', '#339933'];
+    const randomIndex = Math.floor(Math.random() * gradients.length);
+    setBackground(gradients[randomIndex]);
+  };
   return (
-    <div className={s.head__container}>
+    <div className={s.head__container} style={{ background }}>
       <div className={s.banner__container}>
         <div className={s.head__content}>
           <div className={s.head__texts__btn}>
