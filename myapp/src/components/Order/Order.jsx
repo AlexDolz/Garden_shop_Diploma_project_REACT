4f68ae3a6717066ead43f4b6b3a9074f06d69d87
AlexDolz <aleksejdolznikov3@gmail.com>
2023-06-13 14:08:03 +0200

    cart sticky

--- myapp/src/components/Order/Order.jsx
+++ myapp/src/components/Order/Order.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import s from './Order.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import Input from '../UI/Input/Input';
@@ -7,6 +6,16 @@ import Button from '../UI/Button/Button';
 const Order = () => {
   const cart = useSelector(store => store.cart);
   const dispatch = useDispatch();
+  console.log(cart);
+  let totalOrderSum = cart
+    .reduce(
+      (accum, elem) =>
+        accum +
+        elem.count * (elem.discont_price ? elem.discont_price : elem.price),
+      0
+    )
+    .toFixed(2)
+    .replace('.', ',');
 
   const handleKeyDown = event => {
     const allowedKeys = [
@@ -34,7 +43,10 @@ const Order = () => {
       <h3 className={s.order__title}>Order details</h3>
       <div className={s.order__total__sum__wrapper}>
         <p className={s.order__total}>Total</p>
-        <p className={s.order__sum}></p>
+        <p className={s.order__sum}>
+          {totalOrderSum}
+          <span className={s.order__sum__span}>$</span>
+        </p>
       </div>
       <form className={s.order__form}>
         <Input
